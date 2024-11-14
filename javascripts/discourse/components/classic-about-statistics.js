3067bfdb724c2467a6d3d8a3fe61107a29584a61
Jay Pfaffman <jay@literatecomputing.com>
2024-11-14 07:54:02 -0600

    remove extra logging

--- javascripts/discourse/components/classic-about-statistics.js
+++ javascripts/discourse/components/classic-about-statistics.js
@@ -1,4 +1,6 @@
 import Component from "@glimmer/component";
+// html/hbs From https://github.com/discourse/discourse/blob/cc447a1ae34466e6c92ba59a387165f4c213a326/app/assets/javascripts/discourse/app/templates/about.hbs#L142
+
 
 export default class ClassicAboutStatistics extends Component {
   get statistics() {
