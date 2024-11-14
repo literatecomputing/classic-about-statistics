1d3cdd441e5bd80c9972f3c586099b781c9db346
Jay Pfaffman <jay@literatecomputing.com>
2024-11-14 08:15:24 -0600

    make things prettier

--- javascripts/discourse/api-initializers/classic-about-statistics.js
+++ javascripts/discourse/api-initializers/classic-about-statistics.js
@@ -3,15 +3,4 @@ import ClassicAboutStatistics from "../components/classic-about-statistics";
 
 export default apiInitializer("1.38.0", (api) => {
   api.renderInOutlet("about-after-moderators", ClassicAboutStatistics);
-  // api.renderInOutlet("about-wrapper", ClassicAboutStatistics);
-  let myAboutModel = api.container.lookup("controller:about").get('model');
-
-  api.addAboutPageActivity("about", (periods) => {
-    return {
-      icon: "guitar",
-      class: "released-songs",
-      activityText: `${about["today"]} today`,
-      period: "in the last year",
-    };
-  });
 });
--- javascripts/discourse/components/classic-about-statistics.js
+++ javascripts/discourse/components/classic-about-statistics.js
@@ -1,15 +1,8 @@
 import Component from "@glimmer/component";
 // html/hbs From https://github.com/discourse/discourse/blob/cc447a1ae34466e6c92ba59a387165f4c213a326/app/assets/javascripts/discourse/app/templates/about.hbs#L142
 
-
 export default class ClassicAboutStatistics extends Component {
-  get statistics() {
-    return this.args.outletArgs.model.stats;
-  }
-  get model () {
+  get model() {
     return this.args.outletArgs.model;
   }
-  // get shouldShow() {
-  //   return settings.classic_about_statistics_enabled?.length > 0;
-  // }
 }
